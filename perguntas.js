351ae74a09b0fe5344c98be280dcf2d51da39b5b
stadlervinicius <141278048+stadlervinicius@users.noreply.github.com>
2024-11-11 11:18:53 -0300

    ok

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Jogos',
+    'Qual o jogo com mais copias vendidas?',
+    'Minecraft'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
